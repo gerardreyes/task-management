0d63bf94323e2b3c3e16183680d89bcf9fb72b20
Gerard <gerardreyes112@gmail.com>
2023-09-29 02:43:39 +0800

    Added Tasks template
    Added Tasks template

---
+++ task-management-ui/config.js
@@ -0,0 +1,4 @@
+// config.js
+const API_URL = 'http://localhost/api'; // Replace with your actual API URL
+
+export { API_URL };
--- task-management-ui/src/router/index.ts
+++ task-management-ui/src/router/index.ts
@@ -8,23 +8,45 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/login',
     name: 'Login',
-    component: () => import('@/views/LoginView.vue'), // Create this view
+    component: () => import('@/views/LoginView.vue'),
   },
   {
     path: '/register',
     name: 'Register',
-    component: () => import('@/views/RegisterView.vue'), // Create this view
+    component: () => import('@/views/RegisterView.vue'),
   },
   {
     path: '/about',
     name: 'About',
-    component: () => import('@/views/AboutView.vue'), // Check the component import path
+    component: () => import('@/views/AboutView.vue'),
+  },
+  {
+    path: '/tasks',
+    name: 'TaskView',
+    component: () => import('@/views/TaskView.vue'),
+    meta: { requiresAuth: true }, // Add this to indicate that this route requires authentication
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+// Add a global navigation guard to check for authentication
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+
+  // Replace this with your actual authentication logic (e.g., checking if the user is logged in)
+  const isLoggedIn = true; // Example: Assuming the user is logged in
+
+  if (requiresAuth && !isLoggedIn) {
+    // If the route requires authentication and the user is not logged in, redirect to the login page
+    next('/login'); // Replace '/login' with your actual login route
+  } else {
+    // If no authentication is required or the user is logged in, proceed to the route
+    next();
+  }
 });
 
 export default router;
