ee8d1551cecee9bbfddbf3dd87f920b31077d6a6
Gerard <gerardreyes112@gmail.com>
2023-09-29 10:12:17 +0800

    Basic Add task but failing
    Basic Add task but failing

---
+++ task-management-ui/src/store/task.ts
@@ -0,0 +1,30 @@
+// store/task.ts
+import { defineStore } from 'pinia';
+
+interface Task {
+    id: number;
+    title: string;
+    description: string;
+    due_date: string;
+    status: 'TODO' | 'IN-PROGRESS' | 'DONE';
+}
+
+export const useTaskStore = defineStore({
+    id: 'task', // Store ID (you can use a different name if needed)
+    state: () => ({
+        tasks: [] as Task[],
+    }),
+    actions: {
+        // Action to add a task
+        addTask(task: Task) {
+            // Add your logic here to add the task to the state
+            this.tasks.push(task);
+        },
+        // Action to delete a task by ID
+        deleteTask(taskId: number) {
+            // Add your logic here to delete the task from the state
+            this.tasks = this.tasks.filter((task) => task.id !== taskId);
+        },
+        // Add more actions as needed (e.g., editTask, fetchTasks, etc.)
+    },
+});
--- task-management-ui/src/views/TaskView.vue
+++ task-management-ui/src/views/TaskView.vue
@@ -1,5 +1,114 @@
 <template>
   <div>
-    <h2>Tasks</h2>
+    <!-- Add Task Form -->
+    <form @submit.prevent="addTask">
+      <h2>Add Task</h2>
+      <div class="form-group">
+        <label for="title">Title</label>
+        <input v-model="newTask.title" type="text" id="title" required />
+      </div>
+      <div class="form-group">
+        <label for="description">Description</label>
+        <input v-model="newTask.description" type="text" id="description" required />
+      </div>
+      <div class="form-group">
+        <label for="due_date">Due Date</label>
+        <input v-model="newTask.due_date" type="datetime-local" id="due_date" required />
+      </div>
+      <div class="form-group">
+        <label for="status">Status</label>
+        <select v-model="newTask.status" id="status" required>
+          <option value="TODO">TODO</option>
+          <option value="IN-PROGRESS">IN-PROGRESS</option>
+          <option value="DONE">DONE</option>
+        </select>
+      </div>
+      <button type="submit">Add Task</button>
+    </form>
+
+    <!-- Task List -->
+    <h2>Task List</h2>
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+        <div>{{ task.title }}</div>
+        <div>{{ task.description }}</div>
+        <div>{{ task.due_date }}</div>
+        <div>{{ task.status }}</div>
+        <button @click="editTask(task)">Edit</button>
+        <button @click="deleteTask(task.id)">Delete</button>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script setup>
+import { ref, computed } from 'vue';
+import axios from 'axios'; // Import Axios for HTTP requests
+import { useTaskStore } from '@/store/task'; // Import the task store
+import { useRouter } from 'vue-router';
+
+const newTask = ref({
+  title: '',
+  description: '',
+  due_date: '',
+  status: 'TODO',
+});
+
+const taskStore = useTaskStore(); // Use the task store
+const router = useRouter();
+
+const tasks = computed(() => taskStore.tasks);
+
+const addTask = () => {
+  // Make a POST request to save the new task to Laravel
+  axios
+      .post('/tasks', newTask.value)
+      .then((response) => {
+        // Handle success and update your local state with the created task
+        const createdTask = response.data.task;
+        taskStore.addTask(createdTask); // Call the action in the task store to add a task
+        newTask.value = {
+          title: '',
+          description: '',
+          due_date: '',
+          status: 'TODO',
+        };
+      })
+      .catch((error) => {
+        // Handle error, such as displaying an error message
+        console.error('Error adding task:', error);
+      });
+};
+
+const editTask = (task) => {
+  // Navigate to the edit task view with the task ID as a parameter
+  router.push(`/tasks/edit/${task.id}`);
+};
+
+const deleteTask = (taskId) => {
+  taskStore.deleteTask(taskId); // Call the action in the task store to delete a task
+};
+</script>
+
+<style scoped>
+/* Add your CSS styles for the form here */
+.form-group {
+  margin-bottom: 15px;
+}
+/* Add your CSS styles for the list here */
+ul {
+  list-style: none;
+  padding: 0;
+}
+li {
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin: 10px 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+button {
+  cursor: pointer;
+}
+</style>
